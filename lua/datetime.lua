a8eb3e99a35e0219c25f834c68e170c3fd04b6da
sf467 <sf467@outlook.com>
2026-05-14 10:05:30 +0800

    更改 datetime.lua 对触发条件的判断，合并输出

--- lua/datetime.lua
+++ lua/datetime.lua
@@ -4,16 +4,15 @@
 local function translator(input, seg)
    ---@type (string | osdate)[]
    local datetimes = {}
-   if (input == "orq") then
-      table.insert(datetimes, os.date("%Y年-%m月-%d日"))
-      table.insert(datetimes, os.date("%Y%m%d"))
-   elseif (input == "osj") then
-      table.insert(datetimes, os.date("%H时%M分%S秒"))
-      table.insert(datetimes, os.date("%H:%M:%S"))
-   elseif (input == "ors") then
-      table.insert(datetimes, os.date("%Y年%m月%d日%H时%M分%S秒"))
-      table.insert(datetimes, os.date("%Y-%m-%d %H:%M:%S"))
+   if not seg:has_tag("datetime") then
+	   return
    end
+
+   table.insert(datetimes, os.date("%Y年-%m月-%d日"))
+   table.insert(datetimes, os.date("%Y%m%d"))
+   table.insert(datetimes, os.date("%H时%M分%S秒"))
+   table.insert(datetimes, os.date("%H:%M:%S"))
+
    for _, entry in ipairs(datetimes) do
       ---@cast entry string
       yield(Candidate("datetime", seg.start, seg._end, entry, ""))
